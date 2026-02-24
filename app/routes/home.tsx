053f156635655d2769d5687f0ccc1b464b4f3b53
ruhama solomon <rtadesse64@gmail.com>
2026-02-24 08:29:38 +0530

    authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import Navbar from "~/components/Navbar";
+import {resumes} from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "RESUMIND" },
+    { name: "description", content: "smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+      <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center min-h-screen">
+        <Navbar/>
+
+        <section className="mainsection">
+            <div className="page-heading py-16">
+            <h1>TRACK YOUR APPLICATIONS AND RESUME RATINGS</h1>
+            <h2>REVIEW YOUR SUBMISSIONS AND AI POWERED FEEDBACK</h2>
+            </div>
+
+            {resumes.length>0 && (
+              <div className="resumes-section">
+                  {resumes.map((resume) => (
+                      <ResumeCard key={resume.id} resume ={resume}/>
+
+                  ))}
+              </div>
+          )}
+        </section>
+      </main>
+
+
+  );
 }
+
